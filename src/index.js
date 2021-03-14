010cc5071060692f294a5ae5066a56ba20e5db59
Antonina Vasilieva <tonya_verkooijen@mail.ru>
2021-03-14 12:23:44 +0300

    feat: count numbers from 20 to 100

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 module.exports = function toReadable (number) {
-    let arr = [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
-    if (0 <= number < 20) {
-      let i = number;
-      return arr[i]
-  }
+    let arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let arrr = ['twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety '];
+    if (0 <= number && number < 20) {
+      return arr[number]
+  } 
+    if (20 <= number < 100) {
+        let roundNumber= arrr[Math.floor(number/10) - 2];
+        let i = number % 10;
+        if(number%10 !== 0) {
+            return roundNumber + arr[i];
+        }
+        return roundNumber.slice(0, -1);
+    }
 }
